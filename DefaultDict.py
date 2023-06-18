b46abb8ae85d9b03e4b37c4e9828218180e24853
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-06-18 09:56:34 +0000

    DefaultDict

--- DefaultDict.py
+++ DefaultDict.py
@@ -8,7 +8,7 @@ for i in range(1,n+1):
     A[input()].append(i)
 for j in range(1,m+1):
     B[input()].append(j)
- 
+
 for key in B:
     if key in A:
         print(*A[key])
--- Teams.py
+++ Teams.py
@@ -1,3 +1,3 @@
-n = int(input())
-for _ in range(n):
-    for i in range(3)
+# n = int(input())
+# for _ in range(n):
+#     for i in range(3)
