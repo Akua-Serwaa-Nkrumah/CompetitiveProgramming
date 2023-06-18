d7cc537ab8b2ef4ae29a3f2001ac21a597d9280d
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-06-18 09:53:54 +0000

    DefaultDict

---
+++ DefaultDict.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+n, m = map(int, input().split())
+A = defaultdict(list)
+B = defaultdict(list)
+
+for i in range(1,n+1):
+    A[input()].append(i)
+for j in range(1,m+1):
+    B[input()].append(j)
+ 
+for key in B:
+    if key in A:
+        print(*A[key])
+    else:
+        print(-1)
--- PerfectSquares.py
+++ PerfectSquares.py
@@ -2,7 +2,7 @@ import math
  
 def is_perfect_square(n):
     return math.sqrt(n)**2 == n
-
+max_num = float('-inf')
 m = int(input())
 arr = list(map(int,input().split()))
 arr = sorted(arr)
@@ -11,7 +11,9 @@ for i in range(m-1,-1,-1):
         print(arr[i])
         break
     else:
-        print(max)
+        max_num = arr[i]
+        max(max_num,arr[i])
+        print(max_num)
 # max_value = float('-inf')  # Initialize the maximum value as negative infinity
 # for num in arr:
 #     if math.isqrt(num) ** 2 != num:  # Check if the number is not a perfect square
