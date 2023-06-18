51ccfff119a9e0711291ff6e895746a80bc37770
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-06-18 10:36:32 +0000

    DefaultDict

--- DefaultDict.py
+++ DefaultDict.py
@@ -9,8 +9,8 @@ for i in range(1,n+1):
 for j in range(1,m+1):
     B[input()].append(j)
 
-for key in B:
-    if key in A:
-        print(*A[key])
+for letter in B:
+    if letter in A:
+        print(*A[letter])
     else:
         print(-1)
