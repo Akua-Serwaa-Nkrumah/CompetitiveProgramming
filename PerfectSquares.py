0c9c1648db0c1f58013b7b14544353eb07a3514d
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-06-20 10:56:08 +0000

    CommonChars

--- PerfectSquares.py
+++ PerfectSquares.py
@@ -2,21 +2,18 @@ import math
  
 def is_perfect_square(n):
     return math.sqrt(n)**2 == n
-max_num = float('-inf')
+
 m = int(input())
 arr = list(map(int,input().split()))
 arr = sorted(arr)
-for i in range(m-1,-1,-1):
-    if is_perfect_square(arr[i]) != True:
-        print(arr[i])
-        break
+j = 0
+while j < len(arr):
+    if arr[j] < 0:
+        j+=1
+        continue
+    if is_perfect_square(arr[j]) == True:
+        arr.remove(arr[j])
     else:
-        max_num = arr[i]
-        max(max_num,arr[i])
-        print(max_num)
-# max_value = float('-inf')  # Initialize the maximum value as negative infinity
-# for num in arr:
-#     if math.isqrt(num) ** 2 != num:  # Check if the number is not a perfect square
-#         max_value = max(max_value, num)
-
-# print(max_value)
+        j+= 1
+print(max(arr))
+    
