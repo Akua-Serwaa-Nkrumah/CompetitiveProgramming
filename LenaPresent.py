84ee2993faa8c5ff2f2a35a4af0e232306ac8757
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-06-26 12:33:26 +0000

    LenaPresent

--- LenaPresent.py
+++ LenaPresent.py
@@ -2,5 +2,5 @@ n = int(input())
 for i in range(n):
     print(*" "*(n-i),*range(i),*range(i, -1, -1))
 
-for j in range(n, -1, -1):
-    print(*" "*(n-j),*range(j),*range(j, -1, -1))
+for i in range(n, -1, -1):
+    print(*" "*(n-i),*range(i),*range(i, -1, -1))
