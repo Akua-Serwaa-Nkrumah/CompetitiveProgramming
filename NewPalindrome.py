26ea66d00c01a49201b25c976023775407b8c887
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-06-26 02:17:53 +0000

    NewPalindrome

--- NewPalindrome.py
+++ NewPalindrome.py
@@ -1,8 +1,8 @@
 t = int(input())
 for _ in range(t):
     s = input()
-    if len(s)%2 == 1:
-        mid = len(s)//2 
+    mid = len(s)//2
+    if len(s)%2 == 1: 
         before = sorted(s[:mid])
         after =  sorted(s[mid+1:])
         if len(set(before)) == 1 or len(set(after)) == 1:
@@ -14,7 +14,6 @@ for _ in range(t):
                     break
             print("YES")   
     else:
-        mid = len(s)//2 
         before = sorted(s[:mid])
         after =  sorted(s[mid:])
         if len(set(before)) == 1 or len(set(after)) == 1:
