f7b7e05368251de141404dceb600a54f19760d4a
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-06-26 00:12:05 +0000

    ReplaceWithGreatest

--- ReplaceWithGreatest.py
+++ ReplaceWithGreatest.py
@@ -1,7 +1,10 @@
 class Solution(object):
-    def replaceElements(self, arr):
-        for i in range(len(arr)-1):
-            arr[i] = max(arr[i+1:])
-        arr[-1] = -1
+   def replaceElements(self,arr):
+        n = len(arr)
+        max_element = -1
+        for i in range(n-1, -1, -1):
+            temp = arr[i]
+            arr[i] = max_element
+            if temp > max_element:
+                max_element = temp
         return arr
-       
