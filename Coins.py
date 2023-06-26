662195cab27156492c6583a2fca9cfd71322f42f
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-06-26 22:05:49 +0000

    Editted.

--- Coins.py
+++ Coins.py
@@ -1,6 +1,6 @@
 t = int(input())
 for _ in range(t):
-    n,k = list(map(int,input().split()))
+    n,k = map(int,input().split())
     if ((n-k)%2 == 0 or n%k == 0 or n%2 == 0) and n>=k:
         print('YES')
     else: 
