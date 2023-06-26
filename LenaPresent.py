491ae6c203e48fcb296fae34e4022fa5e051302a
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-06-26 12:21:25 +0000

    LenaPresent

--- LenaPresent.py
+++ LenaPresent.py
@@ -1,7 +1,6 @@
 n = int(input())
-print(2)
 for i in range(n):
-    print(*" "*(n-i), *range(i), *range(i, -1, -1))
+    print(*" "*(n-i),*range(i),*range(i, -1, -1))
 
 for i in range(n, -1, -1):
-    print(*" "*(n-i), *range(i), *range(i, -1, -1))
+    print(*" "*(n-i),*range(i),*range(i, -1, -1))
