8aa04212ab4bb787479ec483b9818c1171d0661e
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-07-06 20:51:51 +0000

    InsertDigit and InsertionSort

--- .cph/Codeforces/InsertDigit.py
+++ .cph/Codeforces/InsertDigit.py
@@ -1,30 +1,13 @@
-# t = int(input())
-# def max_digit(n,add_digit, digit):
-#     i = 0
-#     while i < n:
-#         if int(digit[i]) > add_digit >= int(digit[i+2]):
-            
-        
-#     # for i in range(n):
-#     #     if int(digit[i]) < add_digit:
-#     #         return(digit[:i] + str(add_digit) + digit[i:])
-#     #     else: 
-#     #         return (digit[:i+2] + str(add_digit) + digit[i+2:])
-#     # return (digit + str(add_digit))
+t = int(input())
+def max_digit(n,add_digit, digit):   
+    for i in range(n):
+        if int(digit[i]) < add_digit:
+            return(digit[:i] + str(add_digit) + digit[i:])
+    return (digit + str(add_digit))
 
-# for _ in range(t):
-#     n, add_digit = map(int,input().split())
-#     digit = input()
-#     print(max_digit(n,add_digit,digit))
+for _ in range(t):
+    n, add_digit = map(int,input().split())
+    digit = input()
+    print(max_digit(n,add_digit,digit))
 
-# # 765443
-# # 10
-# # 544
-# # 6666
-# # 613579
-# # 987531
-# # 98765443210123456789
-# # 773737
-# # 210000000
-# # 70589590
-# # 8281271277321
+
---
+++ HackerRank/InsertionSort.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    # Write your code here
+    if 1<=n<=1000:
+        temp = arr[-1]
+        for i in range(n-2,-1,-1):
+            arr[i+1] = max(temp,arr[i])
+            print(*arr)   
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
