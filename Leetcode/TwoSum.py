3d395fd1593172550c23349e5c2f0613e3953666
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-07-12 07:35:56 +0000

    Two-pointers

--- Leetcode/TwoSum.py
+++ Leetcode/TwoSum.py
@@ -3,12 +3,19 @@
 #You can return the answer in any order.
 
 class Solution(object):
-    def twoSum(self, nums, target):
-        position = []
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    position.append(i)
-                    position.append(j)
-                    return (position)
-
+    def twoSum(self, numbers, target):
+        left, right = 0, len(numbers)-1
+        while left < right:
+            current_sum = numbers[left]+numbers[right]
+            if current_sum == target:
+                return [left+1,right+1]
+            if current_sum < target:
+                left+=1
+            if current_sum > target:
+                right-=1
+        return []
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
