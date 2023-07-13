df51efe7db0eea2817ee68c2dbcd1300c62ec12e
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-07-13 19:41:46 +0000

    leetcode

--- .cph/Codeforces/MergingArrays.py
+++ .cph/Codeforces/MergingArrays.py
@@ -2,7 +2,7 @@ n, m = map(int,input().split())
 a = list(map(int,input().split()))
 b = list(map(int,input().split()))
 
-# merge_sorted = [0]*(n+m)
+merge_sorted = [0]*(n+m)
 # for i in range(a):
 #     for j in range(b):
 #         if a[i]<=b[]
