1882c93e6db42415eede726252593e49c8f5c506
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-07-14 07:35:36 +0000

    TwoSum Two Pointers

--- Leetcode/TwoSum.py
+++ Leetcode/TwoSum.py
@@ -4,15 +4,15 @@
 
 class Solution(object):
     def twoSum(self, numbers, target):
-        left, right = 0, len(numbers)-1
-        while left < right:
-            current_sum = numbers[left]+numbers[right]
+        index1, index2 = 0, len(numbers)-1
+        while index1 < index2:
+            current_sum = numbers[index1]+numbers[index2]
             if current_sum == target:
-                return [left+1,right+1]
+                return [index1+1,index2+1]
             if current_sum < target:
-                left+=1
+                index1+=1
             if current_sum > target:
-                right-=1
+                index2-=1
         return []
         """
         :type numbers: List[int]
