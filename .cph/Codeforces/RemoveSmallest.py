48ea63cf7a0fe6cfb599d997a6a004f2d3c4a05a
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-07-14 17:05:14 +0000

    two pointers Codeforces

--- .cph/Codeforces/RemoveSmallest.py
+++ .cph/Codeforces/RemoveSmallest.py
@@ -1,16 +1,9 @@
 def RemoveSmallest(arr):
-    compare = [0]*len(arr)
-    for i in range(len(arr)):
-        for j in range(len(arr)):
-            if abs(arr[i]-arr[j]) == 1:
-                minimum = min(arr[i],arr[j])
-                min_index = arr.index(minimum)
-                if compare[min_index] == 0:
-                    compare[min_index] = minimum
-       
-    if compare.count(0) == 1:
-        return "YES"
-    return "NO"
+    arr = sorted(arr)
+    for i in range(len(arr)-1):
+        if abs(arr[i]-arr[i+1]) > 1:
+            return "NO"
+    return "YES"
 
 t = int(input())
 for _ in range(t):
