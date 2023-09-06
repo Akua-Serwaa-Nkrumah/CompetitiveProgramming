c49cadc8ea9143d25ccbc82d38edb3ba8f7c80d4
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-09-06 07:19:11 +0000

    Linked Lists

---
+++ Camp/ProgressSheet/ListCycle.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: [ListNode]) -> bool:
+        slow = head
+        fast = head
+        list_dict = {}
+        while current:
+            if current in list_dict:
+                return True
+            list_dict[current] = True
+
+            current = current.next   
+
+        return False
+        
+
+        
