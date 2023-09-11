a88812207c7f70d847120fad557d8c02263e8b53
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-09-11 14:33:21 +0000

    Contests

--- Camp/ProgressSheet/MinStack.py
+++ Camp/ProgressSheet/MinStack.py
@@ -1,23 +1,25 @@
 class MinStack:
 
     def __init__(self):
-        self.mini = []
+        self.stack = []
+        self.min_stack = []
         
 
     def push(self, val: int) -> None:
-        self.mini.append(val)
-
-    def pop(self) -> None:
-        return self.mini.pop()
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
         
+    def pop(self) -> None:
+        if self.stack and self.min_stack and self.min_stack[-1] == self.stack.pop():
+            self.min_stack.pop()
+
 
     def top(self) -> int:
-        return self.mini[-1]
-        
+        return self.stack[-1] if self.stack else -1
 
     def getMin(self) -> int:
-        return min(self.mini)
-        
+        return self.min_stack[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
