98a8f9e99d912925458e9c8063b598504fe9e5a6
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-09-12 10:37:30 +0000

    Linked Lists

--- Camp/BonusProblems/MaxTwinSum.py
+++ Camp/BonusProblems/MaxTwinSum.py
@@ -1,10 +1,10 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-    def pairSum(self, head: Optional[ListNode]) -> int:
+    def pairSum(self, head:[ListNode]) -> int:
         prev= dummy = ListNode(0,head)
         slow, fast = head, head
         while fast and fast.next:
