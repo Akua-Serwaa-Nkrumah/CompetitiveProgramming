f5faf7eb494deb87087c9a5c1f7c27cf3c919c19
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-10-21 13:53:55 +0000

    Matrices

--- Leetcode/ValidSudoku.py
+++ Leetcode/ValidSudoku.py
@@ -1,35 +1,29 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        def is_valid_unit(unit):
-            seen = set()
-            for digit in unit:
-                if digit != "." and digit in seen:
+class Solution:
+    def isValidSudoku(self, board: [str]) -> bool:
+
+        def checkSubgrid(unit):
+            check = set()
+            for element in unit:
+                if element != '.' and element in check:
                     return False
-                seen.add(digit)
+
+                check.add(element)
+
             return True
 
-        # Check rows
         for row in board:
-            if not is_valid_unit(row):
+            if not checkSubgrid(row):
                 return False
 
-        # Check columns
         for col in zip(*board):
-            if not is_valid_unit(col):
+            if not checkSubgrid(col):
                 return False
+        
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                subGrid = [board[row][col] for row in range(i,i+3) for col in range(j,j+3)]
 
-        # Check sub-boxes
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                sub_box = [
-                    board[row][col] for row in range(i, i + 3) for col in range(j, j + 3)
-                ]
-                if not is_valid_unit(sub_box):
+                if not checkSubgrid(subGrid):
                     return False
 
         return True
-
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
