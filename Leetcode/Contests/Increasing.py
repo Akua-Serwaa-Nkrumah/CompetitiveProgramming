236499b7f7eaee5b93a7db008bd08e237d5c2bc7
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-10-21 13:36:02 +0000

    Competitive Programming

---
+++ Leetcode/Contests/Increasing.py
@@ -0,0 +1,14 @@
+def increase(a):
+    if len(a) == 1:
+        return "YES"
+    
+    if len(set(a)) == n:
+        return "YES"
+    
+    return "NO"
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int,input().split()))
+    print(increase(a))
