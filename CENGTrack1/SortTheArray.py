a978be54e99fcf9f8865ed65550f2c6fa60231e4
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-10-26 20:12:58 +0000

    Codeforces

--- CENGTrack1/SortTheArray.py
+++ CENGTrack1/SortTheArray.py
@@ -1,28 +1,35 @@
 def sortArraySegment(n, arr):
-    start = 0
-    end = 0
+    start = -1
+    end = -1
 
-    if arr == sorted(arr):
-        return (start+1,end+1)
+    i = 0
+    j = n-1
     
-    for start in range(n-1):
-        if arr[start] > arr[start+1]:
-            end = start+1
-        if sorted 
+    while i < n-1 and arr[i] < arr[i+1] :
+        i += 1
+        
+    start = i
     
-    if start == -1:
-        return "yes\n1 1"  # Array is already sorted
+    while j > 0 and arr[j] > arr[j-1]:
+        j -= 1
+            
+    end = j
     
-    # if start != 0 and n != end:
-    #     if arr[end] >= arr[start-1]
-    if  end == n-1 or arr[start] <= arr[end + 1] and (arr[end] >= arr[start-1] and start != 0):
-        return f"yes\n{start + 1} {end + 1}"
+    if start > end:
+        print("yes")
+        print(1,1)
+        return
     
-    return "no"
+    if arr[:start] + arr[start:end+1][::-1] + arr[end+1:] == sorted(arr):
+        print("yes")
+        print (start+1, end+1, sep = ' ')
+        return
+    
+    print ("no")
+    return
 
-# Read input
+
 n = int(input())
 arr = list(map(int, input().split()))
 
-# Call the function and print the result
-print(sortArraySegment(n, arr))
+sortArraySegment(n, arr)
