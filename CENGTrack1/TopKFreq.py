77afdc8eb98bf718b6a70330ca3527022d571b5a
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-11-23 23:44:38 +0000

    Graphs

--- CENGTrack1/TopKFreq.py
+++ CENGTrack1/TopKFreq.py
@@ -1,5 +1,6 @@
 from heapq import *
 from collections import Counter
+<<<<<<< HEAD
 # class Solution:
 #     def topKFrequent(self, words: [str], k: int) -> [str]:
 #         heap = []
@@ -45,11 +46,34 @@ class Solution:
                 if word_count[j] == -mx:
                     words.append(j)
                     
+=======
+class Solution:
+    def topKFrequent(self, words: [str], k: int) -> [str]:
+        heap = []
+        word_count = Counter(words)
+
+        for key in word_count:
+            heap.append(-word_count[key])
+
+        heapify(heap)
+
+        freq = []
+        i = 0
+        while i < k:
+            mx = heappop(heap)
+            words = []
+            for j in word_count:
+                if word_count[j] == -mx and j not in freq:
+                    words.append(j)
+>>>>>>> 04920eb23532d69c41be6dca5a1496ac538cceee
             freq.extend(sorted(words))
             i += 1
         
         return freq[:k]
 
+<<<<<<< HEAD
         
 
+=======
+>>>>>>> 04920eb23532d69c41be6dca5a1496ac538cceee
         
