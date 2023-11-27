39343523ca3a455bbf62f7aac5e316b4cc8d60e1
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-11-27 20:41:15 +0000

    Contest

--- .cph/Codeforces/LessOrEqual.py
+++ .cph/Codeforces/LessOrEqual.py
@@ -1,33 +1,22 @@
-# def LessOrEqual():
-#     n, k = map(int,input().split())
-#     numbers = sorted(map(int,input().split()))
-#     check = []
-#     for i in range(k):
-#         check.append(numbers[i])
-#         numbers[i] = -1
-        
-#     if k > len(numbers):
-#         return -1
+def LessOrEqual():
+    n, k = map(int,input().split())
+    numbers = sorted(map(int,input().split()))
+    check = []
     
-#     for number in numbers[k:]:
-#         if number in check:
-#             return -1
-#     print(numbers.count(numbers[0]))
-# print(LessOrEqual())  
-
-array = [2,"3",5]
-print(array)
-
-
-
-# n, k = map(int,input().split())
-# numbers = sorted(map(int,input().split()))
- 
-# check = []
-# for i in range(n):
-#     if numbers[i] <= k + 1:
-#         check.append(numbers[i])
-# if len(check) == k:
-#     print(k+1)
-# else: 
-#     print(-1)
+    if k == 0:
+        return numbers[0] - 1 if numbers[0] > 1 else -1
+    
+    if k > len(numbers):
+        return -1
+    
+    for i in range(k):
+        check.append(numbers[i])
+        numbers[i] = -1
+    
+    for number in numbers[k:]:
+        if number == check[-1]:
+            return -1
+        
+    return check[-1]
+    
+print(LessOrEqual())  
