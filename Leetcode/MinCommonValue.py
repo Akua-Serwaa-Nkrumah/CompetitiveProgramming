b04e3b9545e363838a123f4edd4c96231f50283b
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2023-12-02 14:14:27 +0000

    Competitive Programming

--- Leetcode/MinCommonValue.py
+++ Leetcode/MinCommonValue.py
@@ -1,5 +1,5 @@
 class Solution:
-    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+    def getCommon(self, nums1: [int], nums2: [int]) -> int:
         i, j = 0, 0
         while i < len(nums1) and j < len(nums2):
             if nums1[i] != nums2[j]:
